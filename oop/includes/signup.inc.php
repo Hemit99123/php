3caba00fd2f8157f3053728b4383794b99ccb0fb
Hemit Patel <hemitvpatel@gmail.com>
2025-08-03 16:44:44 -0400

    add object of signup

--- oop/includes/signup.inc.php
+++ oop/includes/signup.inc.php
@@ -7,4 +7,7 @@ use Oop\Classes\Signup;
 if ($SERVER["REQUEST_METHOD"] == "POST") {
     $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
     $password = filter_input(INPUT_POST,'password', FILTER_SANITIZE_SPECIAL_CHARS);
+
+    $signup = new Signup($username, $password);
+    $signup->signUpUser();
 }
