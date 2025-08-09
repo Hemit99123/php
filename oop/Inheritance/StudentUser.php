f6c636dbc8044e96aa59c2b99a66f3a4288ccb2e
Hemit Patel <hemitvpatel@gmail.com>
2025-08-08 20:42:24 -0400

    Create StudentUser.php

---
+++ oop/Inheritance/StudentUser.php
@@ -0,0 +1,19 @@
+<?php 
+
+include_once __DIR__ . "/User.php";
+
+use Inheritance\User;
+
+class StudentUser extends User 
+{
+  public function joinClass()
+  {
+    
+  }
+
+  public function submitAssignment()
+  {
+    
+  }
+  
+}
